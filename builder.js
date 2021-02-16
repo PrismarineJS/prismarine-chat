fab042ef8c1963bed02afb26a0ee3a55daecccb1
u9g <43508353+u9g@users.noreply.github.com>
2021-02-15 21:45:28 -0500

    add builder.js

---
+++ builder.js
@@ -0,0 +1,36 @@
+class baseMessage {
+  setBold (val) { this.bold = val; return this }
+  setItalic (val) { this.italic = val; return this }
+  setUnderlined (val) { this.underlined = val; return this }
+  setStrikethrough (val) { this.strikethrough = val; return this }
+  setObfuscated (val) { this.obfuscated = val; return this }
+  setColor (val) { this.color = val; return this }
+  setInsertion (val) { this.insertion = val; return this }
+  setText (val) { this.text = val; return this }
+  setClickEvent (action, value) { this.clickEvent = { action, value }; return this }
+  setHoverEvent (action, type, id, name) {
+    this.hoverEvent = { action, contents: { type, id, name: { text: name } } }
+    return this
+  }
+}
+
+class Message extends baseMessage {
+  addComponent (val) {
+    if (!this.with) this.with = []
+    this.with.push(val)
+    return this
+  }
+
+  setTranslate (val) { this.translate = val; return this }
+}
+
+class MessageComponent extends baseMessage {
+  setTranslate (val) { this.translate = val; return this }
+  addComponent (val) {
+    if (!this.with) this.with = []
+    this.with.push(val)
+    return this
+  }
+}
+
+module.exports = (version) => { return { Message, MessageComponent } }
