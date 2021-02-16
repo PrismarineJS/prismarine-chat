f9b312f0d1fc5d508a83b62d5ad4c9c2ad7d8d01
u9g <43508353+u9g@users.noreply.github.com>
2021-02-16 04:07:55 -0500

    del. basemessage, put stuff in message

--- builder.js
+++ builder.js
@@ -1,4 +1,4 @@
-class BaseMessage {
+class Message {
   setBold (val) { this.bold = val; return this }
   setItalic (val) { this.italic = val; return this }
   setUnderlined (val) { this.underlined = val; return this }
@@ -12,9 +12,7 @@ class BaseMessage {
     this.hoverEvent = { action, contents: { type, id, name: { text: name } } }
     return this
   }
-}
 
-class Message extends BaseMessage {
   addTranslateInsertion (val) {
     if (!this.with) this.with = []
     this.with.push(val)
